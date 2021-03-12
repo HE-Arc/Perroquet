a0e61b78cba99d87a1989dfe00cfdc8d9997aa4f
Florian <florian.feuillade@he-arc.ch>
2021-03-12 08:49:49 +0100

    add skeleton of NavigationBar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,42 +2,8 @@
   <v-app>
     <v-system-bar app>
     </v-system-bar>
+    <navigationbar></navigationbar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-sheet
-        color="grey lighten-4"
-        class="pa-4"
-      >
-        <v-avatar
-          class="mb-4"
-          color="grey darken-1"
-          size="64"
-        ></v-avatar>
-
-        <div>john@vuetifyjs.com</div>
-      </v-sheet>
-
-      <v-divider></v-divider>
-
-      <v-list>
-        <v-list-item
-          v-for="[icon, text] in links"
-          :key="icon"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-main>
       <router-view/>
     </v-main>
@@ -45,10 +11,11 @@
 </template>
 
 <script>
+import Navigationbar from "@/components/NavigationBar";
 
 export default {
   name: 'App',
-
+  components: {Navigationbar},
   data: () => ({
     //
   }),
