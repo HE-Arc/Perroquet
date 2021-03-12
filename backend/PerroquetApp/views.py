d47f383a1bd41902e5bdd79d5d217a6826efdcf2
Antoine <antoine.lestrade89@gmail.com>
2021-03-12 09:18:41 +0100

    UserViewSet

--- backend/PerroquetApp/views.py
+++ backend/PerroquetApp/views.py
@@ -31,6 +31,33 @@ class MessageViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
+class UserViewSet(viewsets.ViewSet):
+    """
+    Example empty viewset demonstrating the standard
+    actions that will be handled by a router class.
+
+    If you're using format suffixes, make sure to also include
+    the `format=None` keyword argument for each action.
+    """
+
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
+
 # class MessageList(generics.ListCreateAPIView):
 #     queryset = Message.objects.all()
 #     serializer_class = MessageSerializer
