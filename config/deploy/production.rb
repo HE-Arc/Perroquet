2e29950d0de42bfd3b731b4db39a08c2d38b40d5
Jonatan Baumgartner <me@jojoc4.ch>
2021-03-14 10:57:32 +0100

    capristano ok

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,6 +11,63 @@ server "perroquet.srvz-webapp.he-arc.ch", user: "poweruser", roles: %w{app db we
 set :deploy_to, "/var/www/#{fetch(:application)}"
 
 
+after 'deploy:publishing', 'django:migrate'
+
+namespace :django do
+
+    def venv_path
+        File.join(shared_path, 'env')
+    end
+
+    desc 'Apply migrations'
+    task :migrate do
+        on roles(:web) do |h|
+            execute "cp #{shared_path}/.env #{release_path}/backend/PerroquetProject/.env"
+            execute "source #{venv_path}/bin/activate && python3 #{release_path}/backend/manage.py migrate"
+	    end
+    end
+end
+
+after 'django:migrate', 'uwsgi:restart'
+
+namespace :uwsgi do
+    desc 'Restart application'
+    task :restart do
+        on roles(:web) do |h|
+	        execute :sudo, 'sv reload uwsgi'
+	    end
+    end
+end
+
+after 'deploy:updating', 'python:update_venv'
+
+namespace :python do
+
+    def venv_path
+        File.join(shared_path, 'env')
+    end
+
+    desc 'update venv'
+    task :update_venv do
+        on roles([:app, :web]) do |h|
+            execute "source #{venv_path}/bin/activate && #{venv_path}/bin/pip install -r #{release_path}/backend/requirements.txt"
+        end
+    end
+end
+
+
+after 'deploy:publishing', 'frontend:compile'
+
+namespace :frontend do
+    desc 'compile frontend'
+    task :compile do
+        on roles(:web) do |h|
+	        execute "cd #{release_path}/frontend && npm install && npm run build"
+	    end
+    end
+end
+
+
 
 # role-based syntax
 # ==================
