9464a0d67cb275d85a21b81de8346a37619a3bc0
Florian <florian.feuillade@he-arc.ch>
2021-03-14 19:21:36 +0100

    Design of navbar is done

--- frontend/src/components/NavigationBar.vue
+++ frontend/src/components/NavigationBar.vue
@@ -1,28 +1,19 @@
 <template>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-sheet
-        color="grey lighten-4"
-        class="pa-4"
-      >
-        <v-avatar
-          class="mb-4"
-          color="grey darken-1"
-          size="64"
-        ></v-avatar>
-
-        <div>john@vuetifyjs.com</div>
-      </v-sheet>
+  <v-navigation-drawer app>
+    <v-layout column fill-height>
+      <v-card-title>
+        <v-img :src="require('../assets/perroquet_logo/svg/Logo_DarkMagenta.svg')" height="64" width="64"
+               contain></v-img>
+        <span class="pa-4">Perroquet</span>
+      </v-card-title>
 
       <v-divider></v-divider>
 
       <v-list>
         <v-list-item
-          v-for="[icon, text] in links"
-          :key="icon"
-          link
+            v-for="[icon, text] in links"
+            :key="icon"
+            link
         >
           <v-list-item-icon>
             <v-icon>{{ icon }}</v-icon>
@@ -33,12 +24,38 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer>
+      <v-spacer></v-spacer>
+      <v-list>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-login</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            Login
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-layout>
+
+
+  </v-navigation-drawer>
 </template>
 
 <script>
 export default {
-name: "Navigationbar"
+  name: "Navigationbar",
+
+  data: () => ({
+    links: [
+      ['mdi-home', 'Home'],
+      ['mdi-compass', 'Discover'],
+      ['mdi-account-heart', 'Friends'],
+      ['mdi-cog', 'Settings'],
+
+    ],
+
+  }),
 }
 </script>
 
