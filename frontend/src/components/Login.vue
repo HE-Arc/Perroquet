2c898c3bada5c5cb7e57bcf68fc76f8564d5e78d
Florian <florian.feuillade@he-arc.ch>
2021-03-15 17:35:04 +0100

    Debug submit button text

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -19,9 +19,9 @@
       </v-form>
 
       <v-card-actions>
-        <router-link to="/signin">Login</router-link>
+        <router-link to="/signin">Sign in</router-link>
         <v-spacer></v-spacer>
-        <v-btn type="submit" color="primary" :disabled="!valid">Submit</v-btn>
+        <v-btn type="submit" color="primary" :disabled="!valid">Login</v-btn>
       </v-card-actions>
     </v-card-text>
 
