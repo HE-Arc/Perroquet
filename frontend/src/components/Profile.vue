a4e59844695f9d4050d22e13cc5bcd74daf9f323
Jonatan Baumgartner <me@jojoc4.ch>
2021-04-02 14:01:36 +0200

    début de profile

---
+++ frontend/src/components/Profile.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-card outlined elevation="10">
+          <v-card-title>Sign in</v-card-title>
+          <v-card-text>
+            
+          </v-card-text>
+          <v-card-actions>
+              
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: "Profile",
+
+  data: () => ({
+    
+  }),
+  computed: {
+      profile() {
+          var p = null;
+          this.$store.dispatch("getProfile", this.$route.params.token).then(
+              (p) => {
+                  
+              }
+          );
+      }
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style scoped>
+</style>
