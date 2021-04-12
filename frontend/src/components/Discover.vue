7f5815663f90c6292355ea32bffb248de2ffa3fd
Florian <florian.feuillade@he-arc.ch>
2021-04-12 14:59:38 +0200

    debug username

--- frontend/src/components/Discover.vue
+++ frontend/src/components/Discover.vue
@@ -1,9 +1,9 @@
 <template>
-  <div > <p>{{this.$store.state.messages}}</p>
+  <div >
     <message v-for="message in this.$store.state.messages"
     :key="message.id"
     v-bind:text="message.content"
-    v-bind:author="message.username"
+    v-bind:author="message.user.username"
     v-bind:avatar="message.user.profile.image">
 
     </message>
