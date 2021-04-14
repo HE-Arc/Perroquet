96200412d7deeb07eef14ab5614659341099c09a
Jonatan Baumgartner <me@jojoc4.ch>
2021-04-14 16:00:43 +0200

    miniProfile component

---
+++ frontend/src/components/MiniProfile.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card outlined elevation="10">
+    <v-card-title>
+      <router-link :to="'/profile/'+user.id" class="profileLink">
+        <v-avatar>
+          <v-img :src="user.profile.image" contain></v-img>
+        </v-avatar>
+        <span class="pa-4">{{ user.username }}</span>
+      </router-link>
+    </v-card-title>
+    <v-card-text>{{ user.profile.bio }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "MiniProfile",
+  props: {
+    user:Object,
+  },
+  data: () => ({
+
+  }),
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.profileLink{
+  text-decoration: none;
+  color: inherit;
+}
+
+</style>
