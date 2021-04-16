b94600061df14c4ce63aefe5ce331fbed82a43d9
Antoine <antoine.lestrade89@gmail.com>
2021-04-16 19:16:47 +0200

    Add Sort messages by Hot, Top and New

--- backend/PerroquetApp/urls.py
+++ backend/PerroquetApp/urls.py
@@ -26,6 +26,5 @@ urlpatterns = [
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('token/', obtain_auth_token, name='api_token_auth'),
     # path('token/refresh/', obtain_auth_token, name='api_token_auth'),
-    path('hello/', views.HelloView.as_view(), name='hello'),
     path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
