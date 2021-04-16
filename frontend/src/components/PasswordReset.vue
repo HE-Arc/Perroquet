4311be1ce354fcb44849bf0a2753e65143e382a4
Jonatan Baumgartner <me@jojoc4.ch>
2021-04-16 12:26:52 +0200

    added container on entire app

--- frontend/src/components/PasswordReset.vue
+++ frontend/src/components/PasswordReset.vue
@@ -1,5 +1,4 @@
 <template>
-  <v-container>
     <v-row class="text-center">
       <v-col cols="12">
         <v-card outlined elevation="10">
@@ -26,7 +25,6 @@
         </v-card>
       </v-col>
     </v-row>
-  </v-container>
 </template>
 
 <script>
