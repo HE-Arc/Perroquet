a6db7bacff07900581b926668c3796a04584e342
Jonatan Baumgartner <me@jojoc4.ch>
2021-04-16 12:34:03 +0200

    home page

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <div v-if="!messagesAvailable">
+    No message to show you yet.
+  </div>
+  <div v-if="messagesAvailable">
+    <filters v-on:input="requestMessages()"></filters>
+    <message v-for="message in this.$store.state.messages"
+    :key="message.id"
+    :message="message">
+
+    </message>
+  </div>
+</div>
+
+</template>
+
+<script>
+import Message from "@/components/Message";
+import Filters from "@/components/Filters";
+export default {
+  name: "Home",
+  components: {Filters, Message},
+  computed: {
+    messagesAvailable: function() {
+    // eslint-disable-next-line no-unused-vars
+      for (var k in this.$store.state.messages){
+        return true
+      }
+      return false;
+    }
+  },
+  beforeMount() {
+    this.requestMessages()
+
+  },
+  methods: {
+    requestMessages(){
+this.$store.dispatch("requestHome", "test");
+    }
+  },
+
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
