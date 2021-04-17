3e8adc665ebe8447bb3ae9a75621159d1989be39
Florian <florian.feuillade@he-arc.ch>
2021-04-17 17:20:24 +0200

    Friends display correctly

--- frontend/src/components/Friends.vue
+++ frontend/src/components/Friends.vue
@@ -1,36 +1,40 @@
 <template>
-<div>
-  <v-row><v-col>
-    <new-message v-on:new="requestMessages()"></new-message>
-  </v-col></v-row>
-  <div v-if="!messagesAvailable">
-    No message to show you yet.
-  </div>
-  <div v-if="messagesAvailable">
-    <v-row><v-col>
-      <filters v-on:input="requestMessages()"></filters>
-    </v-col></v-row>
+  <v-container>
+    <v-row>
+      <v-col>
+        <new-message v-on:new="requestMessages()"></new-message>
+      </v-col>
+    </v-row>
+    <v-row v-if="!messagesAvailable">
+      <v-col>
+        No message to show you yet.
+      </v-col>
+    </v-row>
+    <v-row v-if="messagesAvailable">
+      <v-col>
+        <filters v-on:input="requestMessages()"></filters>
+      </v-col>
+    </v-row>
     <v-row v-for="message in this.$store.state.messages" :key="message.id">
       <v-col>
         <message :message="message"></message>
       </v-col>
     </v-row>
-  </div>
-</div>
-
+  </v-container>
 </template>
 
 <script>
 import Message from "@/components/Message";
 import Filters from "@/components/Filters";
 import NewMessage from '@/components/NewMessage.vue';
+
 export default {
   name: "Friends",
-  components: {Filters, Message, NewMessage },
+  components: {Filters, Message, NewMessage},
   computed: {
-    messagesAvailable: function() {
-    // eslint-disable-next-line no-unused-vars
-      for (var k in this.$store.state.messages){
+    messagesAvailable: function () {
+      // eslint-disable-next-line no-unused-vars
+      for (var k in this.$store.state.messages) {
         return true
       }
       return false;
@@ -41,11 +45,10 @@ export default {
 
   },
   methods: {
-    requestMessages(){
-this.$store.dispatch("requestFriends", "test");
+    requestMessages() {
+      this.$store.dispatch("requestFriends", "test");
     }
   },
-
 
 
 }
